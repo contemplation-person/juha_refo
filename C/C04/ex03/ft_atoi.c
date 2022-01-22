af731f5c926439f08dcd9b28007f768c3ac567ff
juha <gkwndud481@naver.com>
2022-01-23 01:04:46 +0900

    c04

---
+++ C/C04/ex03/ft_atoi.c
@@ -0,0 +1,49 @@
+int    check_minus(char *num)
+{
+    int i;
+
+    i = 0;
+    while(*num)
+    {
+        if (*num == '-')
+            i++;
+        num++;
+    }
+    return (i % 2);
+}
+
+int    make_num(char *str)
+{
+    long long int num;
+
+    num = 0;
+    while (*str)
+    {
+        if ((*str == ' ') || (*str == '\f') || (*str == '\n') 
+            || (*str == '\r') || (*str == '\t') || (*str == '\v'))
+            return 0;
+        if (('a' <= *str && *str <= 'z') || ('A' <= *str && *str <= 'Z'))
+            break;
+        if ('0' <= *str && *str <= '9')
+        {
+            num *= 10;
+            num += (*str) - '0';
+        }
+        str++;
+    }
+    return num;
+}
+
+int ft_atoi(char *str)
+{
+    long long int num;
+
+    while ((*str == ' ') || (*str == '\f') || (*str == '\n') 
+        || (*str == '\r') || (*str == '\t') || (*str == '\v'))
+        str++;
+    num = make_num(str);
+    if(check_minus(str))
+        return -num;
+    else
+        return num;
+}
