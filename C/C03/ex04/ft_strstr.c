0d7b5e734402f20e6059b2266292ad8ad3aac9ed
Juyoung Ha <juha@c8r6s13.42seoul.kr>
2022-01-23 14:05:00 +0900

    a

--- C/C03/ex04/ft_strstr.c
+++ C/C03/ex04/ft_strstr.c
@@ -6,34 +6,53 @@
 /*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 19:50:05 by juha              #+#    #+#             */
-/*   Updated: 2022/01/22 15:51:32 by juha             ###   ########.fr       */
+/*   Updated: 2022/01/23 13:58:09 by juha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strlen(char *str)
+{
+	int	cnt;
+
+	cnt = 0;
+	while (*str)
+	{
+		cnt++;
+		str++;
+	}
+	return (cnt);
+}
+
+int	check_str(char *str, char *to_find, int to_find_len)
 {
 	int	i;
 
-	i = 0;
-	if (s1[0] == '\0' && s1[0] == '\0')
-		return (0);
-	while ((s1[i] == s2[i]) && (s1[i] != '\0') && (s2[i] != '\0'))
-		i++;
-	return (s1[i - 1] - s2[i - 1]);
+	i = -1;
+	while (++i < to_find_len)
+	{
+		if (str[i] != to_find[i])
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
 
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
+	int	to_find_len;
 
 	i = -1;
-	if (str[0] == '\0')
-		return (NULL);
-    if(!str[i + 1] && !to_find[i + 1])
-        return (str);
+	to_find_len = ft_strlen(to_find);
+	if (to_find_len == 0)
+		return (str);
 	while (str[++i])
 	{
-		if (ft_strcmp(&(str[i]), to_find) == 0)
-			return (&(str[i]));
+		if (str[i] == to_find[0])
+		{
+			if (check_str((str + i), to_find, to_find_len))
+				return (str + i);
+		}
 	}
 	return (NULL);
 }
--- C/C03/ex05/ft_strlcat.c
+++ C/C03/ex05/ft_strlcat.c
@@ -1,27 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/23 13:58:26 by juha              #+#    #+#             */
+/*   Updated: 2022/01/23 14:01:27 by juha             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-
-unsigned int    ft_strlcat(char *dest, char *src, unsigned int size)
+int	check_len(char *str)
 {
-	unsigned int	cnt;
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
 	unsigned int	i;
 
 	i = 0;
-	cnt = 0;
-
-    while (dest[i])
-        i++;
-    if (i > size)
-    {
-        while(src[cnt])
-        {
-           cnt++;
-        }
-           return (cnt + size);
-    }
-    else
-    {
-        while (src[i++])
-            cnt++;
-    }
-    return (cnt);
+	dest_len = check_len(dest);
+	src_len = check_len(src);
+	if (size <= dest_len)
+		return (size + src_len);
+	else
+	{
+		while (dest[i])
+			i++;
+		while (i + 1 < size)
+		{
+			dest[i] = *src;
+			i++;
+			src++;
+		}
+		dest[i] = '\0';
+		return (dest_len + src_len);
+	}
 }
