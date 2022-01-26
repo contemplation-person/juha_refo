a29717449685b7de000c18e8e11de4618811fcad
Juyoung Ha <juha@c7r2s9.42seoul.kr>
2022-01-26 17:25:11 +0900

    a

---
+++ C/C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,48 @@
+int	ft_sqrt(int nb)
+{
+	int	i;
+
+	i = 1;
+	while (i <= (nb / 2))
+	{
+		if ((i * i) == nb)
+			return (i);
+		if ((i * i) > nb)
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_is_prime(int nb)
+{
+	long int	i;
+
+	if (1 < nb)
+	{
+		i = 1;
+		while (i < ft_sqrt(nb))
+		{
+			i++;
+			if (nb % i == 0)
+				return (0);
+		}
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	int	i;
+
+	i = nb;
+	if (1 > i)
+		return (0);
+	while (1)
+	{
+		if (ft_is_prime(i) == 1)
+			return (i);
+		i++;
+	}
+}
