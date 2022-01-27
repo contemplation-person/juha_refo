7d1a14bad9625bc4805fba121c0a0c5b13887ac1
Juyoung Ha <juha@c8r2s13.42seoul.kr>
2022-01-27 11:49:16 +0900

    a

--- C/C05/ex00/ft_iterative_factorial.c
+++ C/C05/ex00/ft_iterative_factorial.c
@@ -1,12 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juha <juha@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 11:28:47 by juha              #+#    #+#             */
+/*   Updated: 2022/01/27 11:31:52 by juha             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_iterative_factorial(int nb)
 {
 	int	num;
 
-	num = 0;
+	num = nb;
 	if (nb > 1)
 	{
-		num = nb;
-		return (num *= ft_iterative_factorial(nb - 1));
+		while (nb)
+		{
+			num *= nb;
+			nb -= 1;
+		}
+		return (num);
 	}
 	else if ((nb == 1) || (nb == 0))
 		return (1);
--- C/C05/ex08/ft_ten_queens_puzzle.c
+++ C/C05/ex08/ft_ten_queens_puzzle.c
@@ -1,21 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*   ft_ten_queens_puzzle.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juha <juha@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/27 11:29:09 by juha              #+#    #+#             */
-/*   Updated: 2022/01/27 11:30:33 by juha             ###   ########.fr       */
+/*   Created: 2022/01/27 11:30:13 by juha              #+#    #+#             */
+/*   Updated: 2022/01/27 11:30:28 by juha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_fibonacci(int index)
-{
-	if (index > 1)
-		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
-	else if (index < 0)
-		return (0);
-	else
-		return (1);
-}
+int	ft_ten_queens_puzzle(void);
