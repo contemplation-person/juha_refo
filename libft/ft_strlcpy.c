e80388c9d56f619945fe4988dc08889420553030
conteng <contemplation_person@gmail.com>
2022-03-15 01:52:06 +0900

    a

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juha <juha@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: conteng <conteng@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 20:44:54 by juha              #+#    #+#             */
-/*   Updated: 2022/03/14 14:56:38 by juha             ###   ########.fr       */
+/*   Updated: 2022/03/15 01:51:06 by conteng          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,20 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	cnt;
+	size_t	src_len;
 
-	cnt = 0;
 	if (!dst || !src)
 		return (0);
-	while (src[cnt] != '\0' || cnt + 1 < dstsize)
+	src_len = 0;
+	while (src[src_len])
+		src_len++;
+	cnt = 0;
+	while (cnt < src_len || cnt + 1 < dstsize)
 	{
 		dst[cnt] = src[cnt];
 		cnt++;
 	}
 	if (dstsize > 0)
 		dst[cnt] = '\0';
-	return (cnt);//src길이 리턴
+	return (src_len);
 }
