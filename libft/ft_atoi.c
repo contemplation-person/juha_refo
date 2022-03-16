8fcb2006f06611932c78b8ab641583f88190f43c
juha <contemplation.person@gmail.com>
2022-03-16 21:48:37 +0900

    a

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,33 +6,32 @@
 /*   By: juha <juha@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 17:18:30 by juha              #+#    #+#             */
-/*   Updated: 2022/03/16 20:22:15 by juha             ###   ########.fr       */
+/*   Updated: 2022/03/16 21:47:45 by juha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *str)
 {
-	long int	cnt;
+	long long	cnt;
 	int			is_minus;
 
-	while (*str == 32 || (9 <= *str && *str <= 13) || *str == '+' )
+	is_minus = 1;
+	while (*str == 32 || (9 <= *str && *str <= 13))
 		str++;
-	if (*str == '-')
+	if (*str == '-' || *str == '+')
 	{
+		if (*str == '-')
+			is_minus = -1;
 		str++;
-		is_minus = -1;
 	}
 	cnt = 0;
 	while (*str && '0' <= *str && *str <= '9')
 	{
-		cnt *= 10;
-		cnt += ((*str) - 48);
+		cnt = cnt * 10;
+		cnt = cnt + ((*str) - 48);
 		str++;
 	}
 	if (!str)
 		return (0);
-	if (is_minus == -1)
-		return ((int)cnt * is_minus);
-	else
-		return ((int)cnt);
+	return ((int)cnt * is_minus);
 }
--- libft/main.c
+++ libft/main.c
@@ -5,7 +5,7 @@
 int	ft_atoi(const char *str);
 
 int	main(){
-	printf("ft_atoi : %d\n", ft_atoi("\t\v\f\r\n \f-06050"));
-	printf("atoi    : %d\n", atoi("\t\v\f\r\n \f-06050"));
+	printf("ft_atoi : %d\n", ft_atoi("9223372036854775807"));
+	printf("atoi    : %d\n", atoi("9223372036854775807"));
 	return 0;
 }
