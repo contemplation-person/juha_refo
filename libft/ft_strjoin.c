4517ecfc6bdafb1e7845d5640ace2ae1149fa8d2
juha <contemplation.person@gmail.com>
2022-03-18 13:39:30 +0900

    a

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,37 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juha <juha@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/17 17:49:47 by juha              #+#    #+#             */
-/*   Updated: 2022/03/18 12:01:13 by juha             ###   ########.fr       */
+/*   Created: 2022/03/18 12:04:06 by juha              #+#    #+#             */
+/*   Updated: 2022/03/18 13:38:51 by juha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	i;
 	size_t	j;
+	size_t	size;
 	char	*temp;
 
-	if (!s)
+	if (!s1 && s2)
 		return (0);
-	temp = (char *)malloc(len + 1);
+	size = -1;
+	while (s1[++size])
+		;
+	while (s2[++size])
+		;
+	temp = (char *)malloc(size);
 	if (!temp)
 		return (0);
-	if (start >= len)
-	{
-		temp[0] = '\0';
-		return (temp);
-	}
-	i = start;
+	i = -1;
+	while (s1[++i])
+		temp[i] = s1[i];
 	j = 0;
-	while (j < len)
-		temp[j++] = (char)s[i++];
-	temp[j] = '\0';
+	while (s2[j])
+		temp[i++] = s2[j++];
+	temp[i] = '\0';
 	return (temp);
 }
