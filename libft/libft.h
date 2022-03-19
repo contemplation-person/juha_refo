15dc32647551bab011813f5da782c9a60394082b
juha <contemplation.person@gmail.com>
2022-03-19 16:21:39 +0900

    a

--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: conteng <conteng@student.42.fr>            +#+  +:+       +#+        */
+/*   By: juha <juha@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 02:22:52 by conteng           #+#    #+#             */
-/*   Updated: 2022/03/18 23:56:35 by conteng          ###   ########.fr       */
+/*   Updated: 2022/03/19 15:44:06 by juha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,31 @@
 # define LIBFT_H
 # include <stdlib.h>
 
+int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
 int		ft_isalnum(int c);
 char	ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t count);
 void	*ft_memset(void *b, int c, size_t len);
-size_t	ft_strlen(const char	*s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char	*s);
+int		ft_strncmp(const unsigned char *s1, const unsigned char *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, size_t start, size_t len);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
--- libft/main.c
+++ libft/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+char	*ft_strtrim(char const *s1, char const *set);
 
 int	main(){
-	int j = 0;
-	for (int i = 0; i < 3; i++)
-	{
-		j = !j;
-		printf("! : %d\n",j);
-	}
+	char	*s1 = "   \t  \n\n \t\t  \n\n\nHello \t  Please\n Trim me !\n   \n \n \t\t\n  ";
+	char	*s2 = "Hello \t  Please\n Trim me !";
+	char	*ret = ft_strtrim(s1, " \n\t");
+
+	printf("ft : %s\n",ret);
 	return (0);
 }
