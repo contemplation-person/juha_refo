1a9b41602421b23f1c75c8cf72e07a2194f1846b
conteng <contemplation_person@gmail.com>
2022-03-25 00:52:20 +0900

    a

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -1,33 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: conteng <conteng@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/24 22:55:39 by conteng           #+#    #+#             */
-/*   Updated: 2022/03/25 00:46:13 by conteng          ###   ########.fr       */
+/*   Created: 2022/03/25 00:45:08 by conteng           #+#    #+#             */
+/*   Updated: 2022/03/25 00:51:55 by conteng          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	i;
-	char			*str;
 
-	if (!s || !f)
-		return (0);
 	i = 0;
-	str = (char *)malloc(ft_strlen(s) + 1);
-	if (!str)
-		return (0);
 	while (s[i])
 	{
-		str[i] = f(i, s[i]);
+		f(i, s + i);
 		i++;
 	}
-	str[i] = '\0';
-	return (str);
 }
