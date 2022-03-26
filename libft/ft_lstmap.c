21f80a0d22defa94eac305470a6db7e287153aae
conteng <contemplation_person@gmail.com>
2022-03-26 11:46:09 +0900

    a

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juha <juha@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: conteng <conteng@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 19:28:23 by juha              #+#    #+#             */
-/*   Updated: 2022/03/25 19:47:35 by juha             ###   ########.fr       */
+/*   Updated: 2022/03/26 11:43:26 by conteng          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,21 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*first_l;
 	t_list	*l;
 
-	l = ft_lstnew(f(lst->content));
+	first_l = ft_lstnew(f(lst));
+	lst = lst->next;
 	while (lst)
 	{
-		l->next = ft_lstnew(f(lst->content));
-		l = l->next;
+		l = ft_lstnew(f(lst));
+		if (!l)
+		{
+			ft_lstclear(&first_l, del);
+			return (0);
+		}
+		ft_lstadd_back(&first_l, l);
 		lst = lst->next;
 	}
-	return (l);
+	return (first_l);
 }
