82929a61a15b24137b246c126ad1dd4ebc269d5e
juha <contemplation.person@gmail.com>
2022-03-31 15:56:19 +0900

    a

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,89 +6,99 @@
 /*   By: juha <juha@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 13:41:05 by juha              #+#    #+#             */
-/*   Updated: 2022/03/28 21:31:17 by juha             ###   ########.fr       */
+/*   Updated: 2022/03/31 15:47:34 by juha             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <sys/types.h>
 
-static size_t	cnt_word(char *s, char c);
+static char		**make_pp(char *s, char c, size_t cnt);
+static void		free_all(char **pp, size_t cnt);
+static size_t	cnt_word(char *str, char c);
 static char		*make_p(char *s, char c);
-static char		**free_all(char **pp, size_t i);
 
-char	**ft_split(char const *str, char c)
+char	**ft_split(char const *s, char c)
 {
 	char	**pp;
-	char	*s;
-	size_t	i;
+	char	*str;
 
-	s = (char *)str;
-	pp = (char **)malloc(sizeof(char *) * (cnt_word(s, c) + 1));
-	if (!pp)
+	str = (char *)s;
+	if (!str)
 		return (0);
-	i = 0;
-	while (i < cnt_word((char *)str, c))
-	{
-		while (*s && *s == c)
-			s++;
-		if (*s != c)
-		{
-			pp[i] = make_p(s, c);
-			if (!pp[i])
-				return (free_all(pp, i));
-			i++;
-		}
-		while (*s && *s != c)
-			s++;
-	}
-	if (cnt_word(s, c))
-		pp[0] = ft_strdup("");
-	pp[i] = 0;
+	while (*str && *str == c)
+		str++;
+	pp = make_pp(str, c, cnt_word(str, c));
 	return (pp);
 }
 
-static size_t	cnt_word(char *s, char c)
+static size_t	cnt_word(char *str, char c)
 {
+	char	*s;
 	size_t	cnt;
-	size_t	i;
 
+	s = str;
 	cnt = 0;
-	i = 0;
-	while (s[i])
+	while (*str && *str == c)
+		str++;
+	while (*s)
 	{
-		while (s[i] && s[i] == c)
-			i++;
 		cnt++;
-		while (s[i] && s[i] != c)
-			i++;
+		while (*s && *s != c)
+			s++;
+		while (*s && *s == c)
+			s++;
 	}
-	if (s[i - 1] && s[i - 1] == c)
-		return (cnt - 1);
 	return (cnt);
 }
 
 static char	*make_p(char *s, char c)
 {
-	size_t	len;
+	size_t	cnt;
 
-	len = 0;
-	while (s[len] && s[len] != c)
-		len++;
-	return (ft_substr(s, 0, len));
+	cnt = 0;
+	while (s[cnt] && s[cnt] != c)
+		cnt++;
+	return (ft_substr(s, 0, cnt));
 }
 
-static char	**free_all(char **pp, size_t i)
+static void	free_all(char **pp, size_t cnt)
 {
-	size_t	j;
+	size_t	i;
 
-	j = 0;
-	while (j < i)
+	i = 0;
+	while (i < cnt)
 	{
-		free(pp[j]);
-		pp[j++] = 0;
+		free(pp[i]);
+		pp[i++] = 0;
 	}
-	free (pp);
+	free(pp);
 	pp = 0;
-	return (0);
+}
+
+static char	**make_pp(char *s, char c, size_t cnt)
+{
+	size_t	i;
+	char	**pp;
+
+	pp = (char **)malloc(sizeof(char *) * (cnt + 1));
+	if (!pp)
+		return (0);
+	i = 0;
+	while (i < cnt)
+	{
+		pp[i] = make_p(s, c);
+		if (pp[i] == 0)
+		{
+			free_all(pp, i);
+			return (0);
+		}
+		i++;
+		while (*s && *s != c)
+			s++;
+		while (*s && *s == c)
+			s++;
+	}
+	pp[i] = 0;
+	return (pp);
 }
--- libft/main.c
+++ libft/main.c
@@ -3,101 +3,17 @@
 #include <unistd.h>
 #include "libft.h"
 
-static size_t	cnt_word(char *s, char c);
-static char		*make_p(char *s, char c);
-static char		**free_all(char **pp, size_t i);
-
-char	**ft_split(char const *str, char c)
+void	*del(void	*a)
 {
-	char	**pp;
-	char	*s;
-	size_t	i;
-
-	s = (char *)str;
-	pp = (char **)malloc(sizeof(char *) * (cnt_word(s, c) + 1));
-	if (!pp)
-		return (0);
-	i = 0;
-	while (i < cnt_word((char *)str, c))
-	{
-		while (*s && *s == c)
-			s++;
-		if (*s != c)
-		{
-			pp[i] = make_p(s, c);
-			if (!pp[i])
-				return (free_all(pp, i));
-			i++;
-		}
-		while (*s && *s != c)
-			s++;
-	}
-	printf("cnt_word : %zu\n",cnt_word(s, c));
-	if (cnt_word(s, c))
-		pp[0] = ft_strdup("");
-	pp[i] = 0;
-	return (pp);
+	int *x;
+	x = (int *)a;
+	return (void *)x;
 }
 
-static size_t	cnt_word(char *s, char c)
+int	main(void)
 {
-	size_t	cnt;
-	size_t	i;
-
-	cnt = 0;
-	i = 0;
-	while (s[i])
-	{
-		while (s[i] && s[i] == c)
-			i++;
-		cnt++;
-		while (s[i] && s[i] != c)
-			i++;
-	}
-	if (s[i - 1] && s[i - 1] == c)
-		return (cnt - 1);
-	return (cnt);
-}
-
-static char	*make_p(char *s, char c)
-{
-	size_t	len;
-
-	len = 0;
-	while (s[len] && s[len] != c)
-		len++;
-	return (ft_substr(s, 0, len));
-}
-
-static char	**free_all(char **pp, size_t i)
-{
-	size_t	j;
-
-	j = 0;
-	while (j < i)
-	{
-		free(pp[j]);
-		pp[j++] = 0;
-	}
-	free (pp);
-	pp = 0;
-	return (0);
-}
-
-
-int	main()
-{
-	char	s1 = 'a';
-	char	s2[] = "";
-	char	**pp = ft_split(s2, s1);
-
-	printf("\n");
-	for(int i = 0; i < (int)cnt_word(s2, s1);i++)
-		printf("ft   : %s\n", pp[i]);
-
-	char s3[] = "sdfsadfasdf";
-	pp = ft_split(s3, s1);
-	for(int i = 0; i < (int)cnt_word(s3, s1);i++)
-		printf("ft   : %s\n", pp[i]);
+	t_list **a;
+	ft_lstclear(a, del);
+	
 	return (0);
 }
