c4b021190876a7a2a9cb8bb8762fe24526f07341
Juyoung Ha <juha@c2r4s4.42seoul.kr>
2022-04-15 20:54:52 +0900

    a

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: conteng <conteng@student.42.fr>            +#+  +:+       +#+        */
+/*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 20:26:35 by juha              #+#    #+#             */
-/*   Updated: 2022/04/15 02:53:15 by conteng          ###   ########.fr       */
+/*   Updated: 2022/04/15 20:04:25 by juha             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,21 @@
 char	*get_next_line(int fd)
 {
 	static t_list	*lst;
-	unsigned char	*str;
-	int				len;
 
-	str = (char *)malloc(BUFFER_SIZE);
-	if (!str)
+	if (BUFFER_SIZE <= 0)
 		return (0);
-	len = read(fd, str, BUFFER_SIZE);
-	if ((!chk_fd(&lst, fd) && len == 0) || len < 0 || fd < 0 || BUFFER_SIZE < 1)
+	if (check_fd(&lst))
 	{
-		free(str);
-		return (0);
-	}
-	else if (chk_fd(&lst, fd) && len == 0)
-		ft_strjoin(lst->content, str);
-	else if (lst)
-	{
-		if (len == 0)
-			return (free_lst(&lst));
-		else
-			add_lst(&lst, new_lst(&lst, str, fd));
+		if ()
 	}
 	else
-		new_lst(&lst, str, fd);
-	return (return_str(&lst));
+	{
+		
+	}
+		
 }
 
-int	chk_fd(t_list **lst, int fd)
+char	*add_str(t_list **lst)
 {
-	t_list	*temp;
-
-	temp = *lst;
-	while ((*lst)->fd != fd)
-	{
-		if ((*lst)->next == temp)
-			return (0);
-		(*lst) = (*lst)->next;
-	}
-	return (1);
+	
 }
