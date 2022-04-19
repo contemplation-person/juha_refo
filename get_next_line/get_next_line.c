51ba93814ed92bee9650648577fd8d3667f5d180
conteng <68673565+contemplation-person@users.noreply.github.com>
2022-04-20 01:58:43 +0900

    a

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -13,6 +13,11 @@
 #include "get_next_line.h"
 #include <unistd.h>
 
+t_list	*check_fd(t_list *lst, int fd);
+char	*cut_until_null(t_list **lst, int fd);
+char	*ft_substr(char const *s, size_t start, size_t len);
+char	*ft_strdup(const char *s1);
+
 char	*get_next_line(int fd)
 {
 	static t_list	*lst;
@@ -40,6 +45,17 @@ char	*get_next_line(int fd)
 	return (0);
 }
 
+t_list	*check_fd(t_list *lst, int fd)
+{
+	while (lst)
+	{
+		if (lst->fd == fd)
+			return (lst);
+		lst = lst->next;
+	}
+	return (0);
+}
+
 char	*cut_until_null(t_list **lst, int fd)
 {
 	char	*put_str;
@@ -47,14 +63,31 @@ char	*cut_until_null(t_list **lst, int fd)
 	t_list	*temp;
 	int		null_idx;
 
+	temp = *lst;
+	put_str = 0;
 	while (check_fd(temp, fd))
 	{
-		put_str = (*lst)->content;
-		null_idx = ft_strlen(put_str, '\n');
-		
+		null_idx = ft_strlen((const char)temp->content, '\n');
+		put_str = ft_strjoin(put_str, ft_substr((const char)temp->content, 0, null_idx));
+		if (!put_str)
+			return (0);
+		if (null_idx < BUFFER_SIZE)
+		{
+			if (null_idx + 1 == BUFFER_SIZE)
+			{
+				*lst = temp->next;
+				free(temp);
+			}
+			save = ft_substr((const char)temp->content, null_idx, BUFFER_SIZE);
+			free(temp->content);
+			temp->content = save;
+			return (put_str);
+		}
+		*lst = temp->next;
+		free(temp);
+		temp = *lst;
 	}
-
-
+	return (put_str);
 /*	
 	if (is_last_lst(lst, fd))
 	{
@@ -100,5 +133,26 @@ char	*ft_substr(char const *s, size_t start, size_t len)
 	while (j < len)
 		temp[j++] = (char)s[i++];
 	temp[j] = '\0';
+	return (temp);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*temp;
+	size_t	i;
+
+	i = 0;
+	while (s1[i])
+		i++;
+	temp = (char *)malloc(i + 1);
+	if (!temp)
+		return (0);
+	i = 0;
+	while (s1[i])
+	{
+		temp[i] = s1[i];
+		i++;
+	}
+	temp[i] = '\0';
 	return (temp);
 }
