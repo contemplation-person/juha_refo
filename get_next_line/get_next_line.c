cde7f6c330a62e93a48b447d6a5788f0c195f721
conteng <contemplation_person@gmail.com>
2022-05-12 01:01:19 +0900

    a

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
+/*   By: conteng <conteng@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 20:26:35 by juha              #+#    #+#             */
-/*   Updated: 2022/05/11 13:09:21 by juha             ###   ########seoul.kr  */
+/*   Updated: 2022/05/11 23:50:46 by conteng          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,20 @@
 t_success	get_list(t_list **head, t_list **fd_lst, int fd);
 t_success	free_lst(t_list **fd_lst);
 
+char	*get_next_line(int fd)
+{
+	static t_list	*head;
+	t_list			*fd_lst;
+	t_success		chk_success;
 
-// char	*get_next_line(int fd)
-// {
-// 	static t_list	*head;
-// 	t_list			*fd_lst;
-// 	t_success		chk_success;
-
-// 	if (fd < 0 || BUFFER_SIZE <= 0)
-// 		return (0);
-// 	if (!get_list(&head, &fd_lst, fd))
-// 		return (0);
-// 	if (!read_str(&fd_lst, fd)) /*리드를 했으면 리드 반환 아니면 error반환*/
-// 		return (free_lst(fd_lst) >> 1);
-// 	return (ret_str(&fd_lst));
-// }
-
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	if (!get_list(&head, &fd_lst, fd))
+		return (0);
+	if (!read_str(&fd_lst, fd)) /*리드를 했으면 리드 반환 아니면 error반환*/
+		return (free_lst(fd_lst) >> 1);
+	return (ret_str(&fd_lst));
+}
 
 t_success	free_lst(t_list **fd_lst)
 {
@@ -50,6 +48,7 @@ t_success	free_lst(t_list **fd_lst)
 		*fd_lst = 0;
 	return (SUCCESS);
 }
+// good
 
 t_success	get_list(t_list **head, t_list **fd_lst, int fd)
 {
@@ -81,11 +80,42 @@ t_success	get_list(t_list **head, t_list **fd_lst, int fd)
 		*head = *fd_lst;
 	return (SUCCESS);
 }
+//good
+
+t_success	join_str(t_list **fd_lst, char **str, ssize_t read_len)
+{
+	char		*temp;
+	ssize_t		i;
+	t_success	include_line;
+
+	include_line = CONTINUE;
+	temp = (char *)malloc((*fd_lst)->buf_len + read_len);
+	if (!temp)
+		return (ERROR);
+	i = -1;
+	while (++i < (*fd_lst)->buf_len)
+	{
+		temp[i] = (*fd_lst)->buf[i];
+		if (temp[i] == '\n')
+			include_line = SUCCESS;
+	}
+	while (++i % (*fd_lst)->buf_len < read_len)
+	{
+		temp[i] = (*str)[i % (*fd_lst)->buf_len];
+		if (temp[i] == '\n')
+			include_line = SUCCESS;
+	}
+	free(*str);
+	free((*fd_lst)->buf);
+	(*fd_lst)->buf_len = (*fd_lst)->buf_len + read_len;
+	return (include_line);
+}
 
 t_success	read_str(t_list **fd_lst, int fd)
 {
-	char	*str;
-	int		read_len;
+	char		*str;
+	ssize_t		read_len;
+	t_success	state;
 
 	str = (char *)malloc(BUFFER_SIZE);
 	if (!str)
@@ -96,7 +126,15 @@ t_success	read_str(t_list **fd_lst, int fd)
 		free(str);
 		return (ERROR);
 	}
-	if ()
+	if (read_len == 0)
+	{
+		free(str);
+		return (CONTINUE);
+	}
+	state = join_str(fd_lst, &str, read_len);
+	if (state == SUCCESS)
+		return (state);
+	if (state == CONTINUE)
+		return (read_str(fd_lst, fd));
+	return (ERROR);
 }
-/*
-*/
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,8 +6,39 @@
 /*   By: conteng <conteng@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 16:16:46 by juha              #+#    #+#             */
-/*   Updated: 2022/05/09 22:38:36 by conteng          ###   ########seoul.kr  */
+/*   Updated: 2022/05/12 01:00:59 by conteng          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ret_str(t_list **fd_lst)
+{
+	char	*ret;
+	char	*save;
+	size_t	ret_len;
+
+	ret_len = 0;
+	while (ret_len < (*fd_lst)->buf_len)
+	{
+		if ((*fd_lst)->buf[ret_len++] == '\n')
+			break ;
+	}
+	ret = (char *)malloc(ret_len + 1);
+	if (!ret)
+		return (ERROR);
+/*ret 복사 + 끝에 \0*/
+	if ((*fd_lst)->buf_len - ret_len)
+	{
+		save = (char *)malloc((*fd_lst)->buf_len - ret_len);
+		if (!save)
+		{
+			free(ret);
+			return (ERROR);
+		}
+		/*save 복사 (*fd_list)->buf = save & (*fd_lst)->buf_len 넣기*/
+	}
+	else
+		free_lst(fd_lst);
+	return (ret);
+}
