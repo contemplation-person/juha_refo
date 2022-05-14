e701749522c4703a398ed8be35f81faffdf140bc
conteng <68673565+contemplation-person@users.noreply.github.com>
2022-05-14 12:42:12 +0900

    a

--- get_next_line/get_next_line_utils_bonus.c
+++ get_next_line/get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*set_save(t_list **fd_lst, char **ret, ssize_t ret_len)
 {
