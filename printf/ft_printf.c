94c20ba269b64313f9471cc84b26dd86564b8a38
conteng <contemplation_person@gmail.com>
2022-05-20 03:01:53 +0900

    a

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -1,3 +1,33 @@
-#include "ft_printf.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: conteng <conteng@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/20 03:00:07 by conteng           #+#    #+#             */
+/*   Updated: 2022/05/20 03:00:28 by conteng          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_printf(const char *, ...)
+#include "ft_printf.h"
+#include <stdarg.h>
+
+int ft_printf(const char *format, ...)
+{
+    va_list ap;
+    int     args;
+    int     i;
+
+    i = 0;
+    while (format[i])
+        i++;
+    --i;
+    while (--i > -1)
+    {
+        if (format[i] == '%')
+        {
+            if (format[i + 1] == 'c')
+        }
+    }
+    va_start(ap, args);
