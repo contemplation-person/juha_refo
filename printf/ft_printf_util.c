bd4eeebdcbc8594cc39231a9a826dfde87dc9e63
Juyoung Ha <juha@c2r6s4.42seoul.kr>
2022-05-19 19:14:59 +0900

    a

---
+++ printf/ft_printf_util.c
@@ -0,0 +1,16 @@
+#include "ft_printf.h"
+
+int ft_printf(const char *, ...)
+{
+/*
+    c : 
+    s
+    p
+    d
+    i
+    u
+    x
+    X
+    %
+*/
+}
--- success mission/get_next_line/get_next_line_bonus.h
+++ success mission/get_next_line/get_next_line_bonus.h
@@ -6,12 +6,12 @@
 /*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 16:00:51 by juha              #+#    #+#             */
-/*   Updated: 2022/05/12 18:46:23 by juha             ###   ########seoul.kr  */
+/*   Updated: 2022/05/19 16:05:48 by juha             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_H 
+# define GET_NEXT_LINE_BONUS_H 
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 3
