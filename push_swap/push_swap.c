67277b03112cbfa5c6faea19382eacb517005ca7
juha <contemplation.person@gmail.com>
2022-08-24 00:35:07 +0900

    make fuction create_stack

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,16 +6,45 @@
 /*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/27 09:20:14 by juha              #+#    #+#             */
-/*   Updated: 2022/07/13 14:55:21 by juha             ###   ########seoul.kr  */
+/*   Updated: 2022/08/24 00:34:18 by juha             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	create_stack(t_stack a, t_stack b, int argc)
+{
+	int	i;
+	int	size;
+
+	size = argc - 1;
+	a.stack = malloc(sizeof(int) * (size));
+	if (!a.stack)
+	{
+		write_error_message("malloc failed\n");
+		exit(1);
+	}
+	b.stack = malloc(sizeof(int) * (size));
+	if (!b.stack)
+	{
+		write_error_message("malloc failed\n");
+		exit(1);
+	}
+	i = 0;
+	while (i < size)
+	{
+		a.stack[i] = -1;
+		b.stack[i] = -1;
+	}
+}
+
 int	main(int argc, char **argv)
 {
+	t_stack	a;
+	t_stack	b;
 
 	check_error(argc, argv);
-	init_arr(argc, argv, &a, &b);
-	return (sort_arr (&a, &b, cmd_group));
+	create_stack(a, b, argc);
+
+	return ();
 }
--- push_swap/push_swap_utils/cmd.c
+++ push_swap/push_swap_utils/cmd.c
@@ -1,51 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   cmd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/27 11:30:29 by juha              #+#    #+#             */
-/*   Updated: 2022/08/24 00:17:00 by juha             ###   ########seoul.kr  */
+/*   Created: 2022/08/24 00:10:53 by juha              #+#    #+#             */
+/*   Updated: 2022/08/24 00:25:06 by juha             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-# include <stdlib.h>
-# include <unistd.h>
-# include <sys/types.h>
+#include "./push_swap.h"
 
-typedef enum e_cmd
+t_bool	swap(t_stack target)
 {
-	PA = 1,
-	SA,
-	RA,
-	RRA,
-	PB = 10,
-	SB,
-	RB,
-	RRB,
-	RRR = 100,
-	RR,
-	SS,
-}t_cmd;
-
-typedef enum e_bool
-{
-	TRUE = -1,
-	FALSE = 0,
-}t_bool;
-
-typedef struct s_stack_info
-{
-	int	*stack;
 	int	front;
-	int	rear;
-}t_stack;
+	int	*stack;
 
-
-t_bool	check_error(int argc, char **argv);
-t_bool	write_error_message(char *message);
-
-#endif
+	stack = target.stack;
+	front = target.front;
+	
+}
