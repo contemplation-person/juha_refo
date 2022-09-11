9008b0442bed20593f2f7c47290173d0736ed302
Juyoung Ha <juha@c2r6s2.42seoul.kr>
2022-09-12 08:14:17 +0900

    backup go a to b

--- push_swap/sorting/sort_stack.c
+++ push_swap/sorting/sort_stack.c
@@ -6,93 +6,11 @@
 /*   By: juha <juha@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 15:48:14 by juha              #+#    #+#             */
-/*   Updated: 2022/09/11 17:55:39 by juha             ###   ########seoul.kr  */
+/*   Updated: 2022/09/12 08:11:42 by juha             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
-void	go_to_a(t_stack *stack, t_ret **ret, int cnt, int pivot);
-
-void	go_to_b(t_stack *stack, t_ret **ret, int max, int min)
-{
-	int	cnt;
-	int	pivot;
-
-	cnt = max - min + 2;
-	if (max - min < 2)
-	{
-		if (max - min < 2)
-			quick_hardcode(stack, stack->a_top, ret, A);
-		if (max - min < 1)
-			two(stack, stack->a_top, ret, A);
-		while (--cnt)
-			r(stack, ret, RA);
-		return ;
-	}
-	pivot = (max - min) / 3;
-	while (--cnt)
-	{
-		if (stack->a_top->idx < pivot)
-		{
-			p(stack, ret, PB);
-			r(stack, ret, RB);
-		}
-		else if (stack->a_top->idx < pivot * 2)
-			p(stack, ret, PB);
-		else
-			r(stack, ret, RA);
-	}
-	cnt = pivot * 2 + min;
-	while (cnt++ < max)
-	{
-		rr(stack, ret, RRA);
-		p(stack, ret, PB);
-	}
-	go_to_a(stack, ret, max, min + pivot * 2);
-	go_to_a(stack, ret, min + pivot * 2 - 1, min + pivot);
-	go_to_a(stack, ret, min + pivot - 1, min);
-}
-
-void	go_to_a(t_stack *stack, t_ret **ret, int max, int min)
-{
-	int	cnt;
-	int	pivot;
-
-	cnt = max - min + 2;
-	if (max - min < 2)
-	{
-		if (max - min < 2)
-			quick_hardcode(stack, stack->b_top, ret, B);
-		if (max - min < 1)
-			two(stack, stack->b_top, ret, B);
-		while (--cnt)
-			p(stack, ret, PB);
-		return ;
-	}
-	pivot = (max - min) / 3;
-	while (--cnt)
-	{
-		if (stack->b_top->idx > pivot)
-		{
-			p(stack, ret, PA);
-			r(stack, ret, RA);
-		}
-		else if (stack->b_top->idx > pivot * 2)
-			p(stack, ret, PA);
-		else
-			r(stack, ret, RB);
-	}
-	cnt = pivot * 2 + min;
-	while (cnt++ < max)
-	{
-		rr(stack, ret, RRB);
-		p(stack, ret, PA);
-	}
-	// 여기 방향 고쳐!!!
-	go_to_a(stack, ret, max, min + pivot * 2);
-	go_to_a(stack, ret, min + pivot * 2 - 1, min + pivot);
-	go_to_a(stack, ret, min + pivot - 1, min);
-}
 
 void	quick_hardcode(t_stack *stack, t_stack_node *top, \
 t_ret **ret, t_s_name name)
