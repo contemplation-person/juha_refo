477cba2252cc399b6a3881bb8951c6313f031894
Juyoung Ha <juha@c10r1s2.42seoul.kr>
2023-04-29 21:34:56 +0900

    temp

--- CPP/CPP06/ex02/Base.cpp
+++ CPP/CPP06/ex02/Base.cpp
@@ -1,22 +1,25 @@
 #include "Base.hpp"
 #include <cstdlib>
 #include <iostream>
+#include <string>
 
 Base::~Base(){}
 A::~A(){}
 B::~B(){}
+C::~C(){}
 
 Base * generate(void)
 {
-	int i = rand() % 3;
+    std::srand(time(NULL));
+    int i = std::rand() % 3;
 	switch (i)
 	{
 	case 0:
-		return new A;
+		return (new A());
 	case 1:
-		return new B;
+		return (new B());
 	default:
-		return new C;
+		return (new C());
 	}
 }
 
@@ -26,28 +29,28 @@ void identify(Base *p)
 	B *b = dynamic_cast<B*>(p);
 	C *c = dynamic_cast<C*>(p);
 	if (a)
-		std::cout << "Pointer: A" << std::endl;
+		std::cout << "Pointer\t\t: A - " << a << std::endl;
 	else if (b)
-		std::cout << "Pointer: B" << std::endl;
+		std::cout << "Pointer\t\t: B - " << b << std::endl;
 	else
-		std::cout << "Pointer: C" << std::endl;
+		std::cout << "Pointer\t\t: C - " << c << std::endl;
 }
 
 void identify(Base &p)
 {
 	try {
-		A &a = dynamic_cast<A&>(p);
-		std::cout << "Reference: A" << std::endl;
+		A& a = dynamic_cast<A&>(p);
+		std::cout << "Reference\t: A - " << &a << std::endl;
 	} catch (std::bad_cast) {
 	}
 	try {
 		B &b = dynamic_cast<B&>(p);
-		std::cout << "Reference: B" << std::endl;
+		std::cout << "Reference\t: B - " << &b << std::endl;
 	} catch (std::bad_cast) {
 	}
 	try {
 		C &c = dynamic_cast<C&>(p);
-		std::cout << "Reference: C" << std::endl;
+		std::cout << "Reference\t: C - " << &c << std::endl;
 	} catch (std::bad_cast) {
 	}
 }
--- CPP/CPP06/ex02/Base.hpp
+++ CPP/CPP06/ex02/Base.hpp
@@ -6,36 +6,24 @@ class Base
 {
 public:
 	virtual ~Base();
-private:
-	Base& operator=(const Base& b);
-	Base(const Base& b);
 };
 
 class A : public Base
 {
 public:
 	~A();
-private:
-	A& operator=(const A& a);
-	A(const A& a);
 };
 
 class B : public Base
 {
 public:
 	~B();
-private:
-	B& operator=(const B& b);
-	B(const B& b);
 };
 
 class C : public Base
 {
 public:
 	~C();
-private:
-	C& operator=(const C& c);
-	C(const C& c);
 };
 
 Base * generate(void);
