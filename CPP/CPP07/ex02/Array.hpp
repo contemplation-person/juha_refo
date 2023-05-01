6ba5b8d731aec26ba21a75bc5f4def64c8d5f3a6
juha <contemplation.person@gmail.com>
2023-05-02 04:16:02 +0900

    CONTINUE

--- CPP/CPP07/ex02/Array.hpp
+++ CPP/CPP07/ex02/Array.hpp
@@ -2,5 +2,17 @@
 #ifndef __ARRAY_HPP__
 #define __ARRAY_HPP__
 
+template<class T> 
+class Array
+{
+public:
+    Array();
+    ~Array();
+    Array(const Array& arr);
+    const Array& operator=(const Array& arr);
+
+private:
+    int size = 0;
+};
 
 #endif
