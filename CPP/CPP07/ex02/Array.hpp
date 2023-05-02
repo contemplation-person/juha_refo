9f6571126ba2da2b84958cf2f16f4be5f39f2fe8
Juyoung Ha <juha@c10r1s2.42seoul.kr>
2023-05-02 22:45:38 +0900

    CONTINUE

--- CPP/CPP07/ex02/Array.hpp
+++ CPP/CPP07/ex02/Array.hpp
@@ -2,17 +2,112 @@
 #ifndef __ARRAY_HPP__
 #define __ARRAY_HPP__
 
+#include <cstdlib>
+
 template<class T> 
 class Array
 {
 public:
     Array();
-    ~Array();
+    Array(unsigned int ui); //
+    virtual ~Array();
     Array(const Array& arr);
-    const Array& operator=(const Array& arr);
+    T operator=(T t);
+    const Array& operator=(const Array& arr) const;
+    void* operator new[](std::size_t ui);
+    T operator[](std::size_t ui);
+    
+    int size();
+
 
 private:
-    int size = 0;
+    T*  _arr;
+    int _size;
 };
+
+template<class T>
+Array<T>::Array(void)
+    : _size(0)
+{
+    this->_arr = new T[0];
+}
+
+template<class T>
+Array<T>::~Array(void) { }
+
+#include <climits>
+#include <stdexcept>
+#include <iostream>
+
+template<class T>
+Array<T>::Array(unsigned int ui) 
+    : _size(ui)
+{
+    try
+    {
+        if (static_cast<int>(ui) > INT_MAX)
+            throw std::out_of_range("out of range");
+        this->_arr = new T[ui];
+        for (int i = 0; i < ui; i++)
+        {
+            _arr[i] = 0;
+        }
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+}
+
+template<class T>
+Array<T>::Array(const Array& arr) 
+{
+    this = arr;
+}
+
+template<class T>
+T Array<class T> operator=(T t)
+{
+
+}
+
+template<class T>
+const Array<T>& Array<T>::operator=(const Array& arr) const 
+{
+    if (this != &arr)
+    {
+        this->_arr = new T[arr._size];
+        for (int i = 0; i < arr._size;i++)
+        {
+            this->_arr[i] = arr[i];
+        }
+        this->_size = arr._size;
+    }
+    return *this; 
+}
+    
+template<class T>
+void* Array<T>::operator new[](std::size_t ui) // std::size_t를 써야 한다고 공식문서에 나와있음.
+{
+    Array tmp(ui);
+    return tmp;
+}
+
+template<class T>
+T Array<T>::operator[](std::size_t ui)
+{
+    try
+    {
+        if (static_cast<int>(ui) > INT_MAX)
+            throw std::out_of_range("out of range");
+        return this->_arr[ui];
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+}
+
+template<class T>
+int Array<T>::size()
+{
+    return this->_size;
+}
 
 #endif
--- CPP/CPP07/ex02/main.cpp
+++ CPP/CPP07/ex02/main.cpp
@@ -1,15 +1,53 @@
-#include "iter.hpp"
+#include <iostream>
+#include <Array.hpp>
 
-int main( void ) {
-    int intArr[3] ={1, 2, 3};
-    double doubleArr[3] ={1.1, 2.1, 3.1};
-    char charArr[3] ={'a', 'b', 'c'};
-    int arrLen = 3;
+#define MAX_VAL 750
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
 
-    std::cout << "===int===" << std::endl;
-    iter(intArr, arrLen, callArr);
-    std::cout << "===double===" << std::endl;
-    iter(doubleArr, arrLen, callArr);
-    std::cout << "===char===" << std::endl;
-    iter(charArr, arrLen, callArr);
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
 }
