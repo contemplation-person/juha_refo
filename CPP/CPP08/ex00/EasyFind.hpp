45070fd62c9d07dbb1fc81d7f4a3142d31043e16
Juyoung Ha <juha@c10r1s2.42seoul.kr>
2023-05-06 22:46:46 +0900

    temp

---
+++ CPP/CPP08/ex00/EasyFind.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef __EASY_FIND__
+#define __EASY_FIND__
+
+template <class T>
+class EasyFind {
+public:
+    EasyFind(T& t, const int& integer);
+private:
+    EasyFind();
+    ~EasyFind();
+    EasyFind(const EasyFind& e);
+    const EasyFind& operator=(const EasyFind& e);
+};
+
+template <class T>
+EasyFind<T>::EasyFind(T& t, const int& integer)
+{
+    int
+    int []
+    container
+};
+#endif 
