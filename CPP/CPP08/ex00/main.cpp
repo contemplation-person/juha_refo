e39d918cb474a5e12d71494f33e744df9e22aa52
juha <contemplation.person@gmail.com>
2023-05-11 10:22:03 +0900

    rename && you must be fix main

--- CPP/CPP08/ex00/main.cpp
+++ CPP/CPP08/ex00/main.cpp
@@ -10,8 +10,8 @@ int main(void)
     std::vector<int> v;
     std::list<int> l = {1, 2, 3, 4};
     std::deque<int> d = {1, 2, 3, 4};
-    easyfind(v, 1);
-    std::cout << *easyfind(l, 1) << std::endl;
-    std::cout << *easyfind(d, 2) << std::endl;
+    easyfind<int>(v, 1);
+    std::cout << *easyfind<int>(l, 1) << std::endl;
+    std::cout << *easyfind<int>(d, 2) << std::endl;
     return 0;
 }
