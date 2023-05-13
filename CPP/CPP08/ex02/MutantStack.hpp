fa4259edf3809d102706dc9570512655b5b01032
juha <contemplation.person@gmail.com>
2023-05-13 10:39:48 +0900

    make base

--- CPP/CPP08/ex02/MutantStack.hpp
+++ CPP/CPP08/ex02/MutantStack.hpp
@@ -7,10 +7,32 @@ template <typename T>
 class MutantStack : public std::stack<T>
 {
 public:
-    
-private:
-    
+	MutantStack();
+	MutantStack(const MutantStack<T> &stack);
+	MutantStack<T> &operator=(const MutantStack<T> &stack);
+	virtual ~MutantStack();
 };
 
+template <typename T>
+MutantStack<T>::MutantStack() 
+	: std::stack<T>()
+{ }
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack<T> &stack)
+	: std::stack<T>(stack)
+{ }
+
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack<T> &stack)
+{
+	if (this != &stack)
+		std::stack<T>::operator=(stack);
+	return (*this);
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack()
+{ }
 
 #endif
