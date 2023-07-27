bc509cd1bf9686db7cd261257a831925f1215981
juha <contemplation.person@gmail.com>
2023-07-27 19:51:25 +0900

    [WIP] init inception

---
+++ inception/srcs/requirements/nginx/tools/certification.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+openssl genrsa -out /test/selfKey.key 1024 
+openssl req -new -key /test/selfKey.key -out /test/selfCsr.csr << done
+ko
+seoul
+seoul
+42
+unknown
+common
+empty
+
+
+done
