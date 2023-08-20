86943f62f6e57da9aefc7f9aed52de1b42ebb507
juha <contemplation.person@gmail.com>
2023-08-20 17:54:08 +0900

    mariadb dockerfile done but don't operation docker compose connet

--- inception/srcs/requirements/mariadb/setup.sh
+++ inception/srcs/requirements/mariadb/setup.sh
@@ -4,14 +4,8 @@ set -e
 
 if [[ ! -d "/var/lib/mysql/mysql" ]]; then 
     /usr/bin/mariadb-install-db --skip-test-db 
+    mariadbd --bootstrap < setup_table
+    
 fi
-
-mariadbd --bootstrap << EOF
-FLUSH PRIVILEGES;
-CREATE DATABASE WordPressDB; 
-CREATE USER juha@localhost IDENTIFIED BY 'helloworld'; 
-GRANT ALL ON WordPressDB.* TO juha@localhost IDENTIFIED BY 'helloworld' WITH GRANT OPTION; 
-FLUSH PRIVILEGES;
-EOF
 
 mariadbd
--- inception/srcs/requirements/wordpress/tools/wp-config.php
+++ inception/srcs/requirements/wordpress/tools/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'mysql' );
+define( 'DB_NAME', 'WordPressDB' );
 
 /** Database username */
 define( 'DB_USER', 'juha' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'helloWorld' );
+define( 'DB_PASSWORD', 'helloworld' );
 
 /** Database hostname */
-define( 'DB_HOST', 'mariadb:<strong>3306' );
+define( 'DB_HOST', '0.0.0.0:<strong>3306' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
