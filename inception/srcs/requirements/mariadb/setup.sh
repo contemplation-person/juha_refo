576899fb0df36dc9c36c788ceb4444b34d9adcb4
juha <contemplation.person@gmail.com>
2023-08-22 21:55:23 +0900

    what the fuck

--- inception/srcs/requirements/mariadb/setup.sh
+++ inception/srcs/requirements/mariadb/setup.sh
@@ -1,11 +1,7 @@
 #!/bin/bash -x
 
 set -e
-
-if [[ ! -d "/var/lib/mysql/mysql" ]]; then 
-    /usr/bin/mariadb-install-db --skip-test-db 
-    mariadbd --bootstrap < setup_table
-    
-fi
+/usr/bin/mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
+mariadbd --bootstrap < setup_table
 
 mariadbd
--- inception/srcs/requirements/wordpress/tools/mkdir.sh
+++ inception/srcs/requirements/wordpress/tools/mkdir.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-# 확인할 디렉토리 이름
-dir_name="/wp/php_wp"
-
 # 디렉토리 존재 여부 확인
 if [ -d "$dir_name" ]; then
     echo "디렉토리 '$dir_name'가 이미 존재합니다."
 else
     echo "디렉토리 '$dir_name'가 존재하지 않습니다."
+
+
     mkdir -p $dir_name
     wp core download --path=$dir_name --allow-root
-    mv /wp-config.php $dir_name
+    # exist wordpress config check website
+
+    wp config create --dbname=$dbname --dbuser=$dbuser --dbpass=$dbpass --dbhost=$dbhost --path=$dir_name --allow-root
+
+    wp core install --url=$url --admin_user=$admin --admin_password=$adminPassword --admin_email=$adminEmail --path=$dir_name --skip-email --allow-root
+	wp user create $newUser $newEmail --path=$dir_name --role=author --user_pass=$newPassword --allow-root
+
+    chown -R www-data:www-data $dir_name
+# erase this
+# mv /wp-config.php $dir_name
 fi
 
 php-fpm7.4 -F
