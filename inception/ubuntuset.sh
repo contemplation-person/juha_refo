3cefeca7b471720e6116b1985b84526783f2e1d5
juha <contemplation.person@gmail.com>
2023-08-28 20:56:14 +0900

    feat: docker setting done

---
+++ inception/ubuntuset.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo su - << done
+juha
+done
+sudo echo "juha  ALL=(ALL) ALL" >> /etc/sudoers
+
+sudo apt update && sudo apt upgrade -y && sudo apt install -y curl openssh-server vim << done
+ssh-keygen << done
+
+
+
+done
+sudo echo "Port 4242" >> /etc/ssh/sshd_config
+sudo ufw allow 4242 
+exit
+sudo apt update << done
+juha
+done
+
+sudo echo "juha.42.fr" >> /etc/hosts
+#install chrome
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i ~/Downloads/google-chrome-stable_current_amd64.deb
+
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo chmod +x get-docker.sh
+sudo sh ./get-docker.sh --dry-run
