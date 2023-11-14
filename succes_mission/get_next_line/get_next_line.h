7703407c55d9aaf4e3dfee864782cf572f270e94
conteng <contemplation.person@gmail.com>
2023-11-14 16:41:27 +0900

    refactor: 헤더 정리

--- succes_mission/get_next_line/get_next_line.h
+++ succes_mission/get_next_line/get_next_line.h
@@ -14,39 +14,24 @@
 # define GET_NEXT_LINE_H 
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 3
+#  define BUFFER_SIZE 1024
 # endif
 
-# include <stdlib.h>
+# include <limits.h>
 # include <sys/types.h>
+# include <stdlib.h>
 # include <unistd.h>
 
-typedef enum e_success
-{
-	ERROR = 0,
-	SUCCESS,
-	CONTINUE
-}t_success;
+# define BUFFER_SIZE 1024
 
-typedef struct s_gnl_list
+
+typedef struct s_gnl
 {
-	ssize_t				fd;
-	ssize_t				buf_len;
-	char				*buf;
-	struct s_gnl_list	*next;
-	struct s_gnl_list	*prev;
-}t_list;
+	int 				fd;
+	char				*rb;
+	int					rb_capacity;
+}t_gnl;
 
 char		*get_next_line(int fd);
-t_success	join_str(t_list **fd_lst, char **str, ssize_t read_len);
-t_success	read_str(t_list **fd_lst, int fd);
-char		*set_save(t_list **fd_lst, char **ret, ssize_t ret_len);
-ssize_t		get_ret_len(t_list **fd_lst);
-
-char		*ret_str(t_list **head, t_list **fd_lst);
-void		*ft_memcpy(void *dst, const void *src, size_t n);
-t_success	free_lst(t_list **head, t_list **fd_lst);
-t_success	get_list(t_list **head, t_list **fd_lst, int fd);
-void		set_fd_list(t_list **fd_lst, t_list *prev, int fd);
 
 #endif
