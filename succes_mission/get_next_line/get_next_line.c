ec7de0888f5ba4d2b25f75e16f3085635b97a14b
conteng <contemplation.person@gmail.com>
2023-11-17 01:10:18 +0900

    refactor: 리펙토링 완성!

--- succes_mission/get_next_line/get_next_line.c
+++ succes_mission/get_next_line/get_next_line.c
@@ -12,14 +12,12 @@
 
 #include "get_next_line.h"
 
-static ssize_t ft_strlen(const char *s, unsigned char target_char)
+static size_t ft_strlen(const char *s, unsigned char target_char)
 {
-	ssize_t i;
+	size_t i;
 
 	i = 0;
-	if (s == NULL)
-		return (0);
-	while (s[i] && target_char != s[i])
+	while (s && s[i] && target_char != s[i])
 		i++;
 	return (i);
 }
@@ -39,42 +37,84 @@ static void	*ft_memcpy(void *dst, const void *src, size_t n)
 	return (dst);
 }
 
-int str_join(t_gnl *gnl, int gnl_fd, char *tmp_str)
+static int str_join(char **dst, int dst_len, char *src, int src_len)
 {
 	char *sum_buf;
 	int sum_len;
-	int	tmp_len;
-	
-	tmp_len = ft_strlen(tmp_str, '\0');
-	sum_len = tmp_len + gnl[gnl_fd].rb_capacity;
+
+	if (!dst_len && !src_len)
+		return 0;
+	sum_len = dst_len + src_len;
 	sum_buf = malloc(sum_len + 1);
 	if (!sum_buf)
 		return (-1);
-	ft_memcpy(sum_buf, gnl[gnl_fd].rb, gnl[gnl_fd].rb_capacity);
-	ft_memcpy(sum_buf[gnl[gnl_fd].rb_capacity], tmp_str, tmp_len);
+	ft_memcpy(sum_buf, *dst, dst_len);
+	ft_memcpy(&sum_buf[dst_len], src, src_len);
 	sum_buf[sum_len] = '\0';
-	free(gnl[gnl_fd].rb);
-	gnl[gnl_fd].rb = sum_buf;
-	gnl[gnl_fd].rb_capacity = sum_len;
+	free(*dst);
+	*dst = sum_buf;
 	return sum_len;
+}
+
+static char *pass_one_line(t_gnl *gnl, int fd_index)
+{
+	char *ret_str;
+	char *save_str;
+	int line_len;
+	int len;
+	
+	ret_str = NULL;
+	save_str = NULL;
+	line_len = ft_strlen(gnl[fd_index].rb, '\n');
+	if (line_len++ != gnl[fd_index].rb_capacity)
+	{
+		len = str_join(&ret_str, 0, gnl[fd_index].rb, line_len);
+		if (len == -1)
+			return (NULL);
+		len = str_join(&save_str, 0, &gnl[fd_index].rb[line_len], gnl[fd_index].rb_capacity - len);
+		if (len == -1)
+		{
+			free(ret_str);
+			free(save_str);
+			len = 0;
+			return (NULL);
+		}
+		free(gnl[fd_index].rb);
+	}
+	else
+	{
+		ret_str = gnl[fd_index].rb;
+		save_str = NULL;
+		len = 0;
+	}
+	gnl[fd_index].rb = save_str;
+	gnl[fd_index].rb_capacity = len;
+	return (ret_str);
 }
 
 char *get_next_line(int fd)
 {
 	static t_gnl gnl[OPEN_MAX];
-	char tmp_str[BUFFER_SIZE];
-	int gnl_fd = fd - 1;
+	char tmp_str[BUFFER_SIZE + 1];
+	int fd_index = fd - 1;
 	int read_len;
 
 	if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE < 1)
 		return (NULL);
-	read_len = read(fd, tmp_str, BUFFER_SIZE - 1);
+	read_len = read(fd, tmp_str, BUFFER_SIZE);
 	while (read_len > 0)
 	{
 		tmp_str[read_len] = '\0';
-		str_join(gnl, gnl_fd, tmp_str);
+		read_len = str_join(&gnl[fd_index].rb, gnl[fd_index].rb_capacity, tmp_str, ft_strlen(tmp_str, '\0'));
+		if (read_len == -1)
+		{
+			free(gnl[fd_index].rb);
+			gnl[fd_index].rb = NULL;
+			gnl[fd_index].rb_capacity = 0;
+			return (NULL);
+		}
+		gnl[fd_index].rb_capacity = read_len;
 		read_len = read(fd, tmp_str, BUFFER_SIZE);
 	}
-	// line
-	return ();
+	return (pass_one_line(gnl, fd_index));
 }
