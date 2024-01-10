029d26e43fa3010ee840282eb42f2d79aef4223d
juha <contemplation.person@gmail.com>
2024-01-10 18:37:21 +0900

    chore: expire and delete

--- success_mission/pong-nyan/secrets/generate_ssl_cert.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048\
-  -keyout /etc/ssl/certs/localhost.key\
-  -out /etc/ssl/certs/localhost.crt\
-  -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Lee/CN=localhost"
