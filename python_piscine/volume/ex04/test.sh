248cd03fb4ff465dfa650bd0a3343cd3558cb96b
Ha Ju Young <contemplation.person@gmail.com>
2025-12-08 20:12:39 +0900

    [FEAT] python ex04
     # learning point
    * def
    * try
    * exept
    * raise
    * sys function

---
+++ python_piscine/volume/ex04/test.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+python3 whatis.py 14
+echo ""
+python3 whatis.py -5 
+echo ""
+python3 whatis.py
+echo ""
+python3 whatis.py 0
+echo ""
+python3 whatis.py HI!
+echo ""
+python3 whatis.py 13 5
+
---
+++ python_piscine/volume/ex04/whatis.py
@@ -0,0 +1,25 @@
+import sys
+
+def ErrorMessage(context):
+    print("AssertionError:", context)
+
+argc = len(sys.argv)
+
+try:
+    if argc == 1:
+        sys.exit()
+    elif argc > 2:
+        raise AssertionError("more than one argument is provided")
+
+    number = int(sys.argv[1])
+
+
+    if (number % 2):
+        print("I'm Odd.")
+    elif (number % 2) == 0:
+        print("I'm Even.")
+
+except AssertionError as e:
+    ErrorMessage(e)
+except ValueError:
+    ErrorMessage("argument is not an integer")
